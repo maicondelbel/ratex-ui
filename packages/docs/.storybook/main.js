e0f51b000041c1a4fa6844b665860b5dadd7d3b2
Maicon P. Del Bel <maicon.delbel@gmail.com>
2022-11-18 20:26:07 -0300

    fix basedir

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/ignite-design-system/'
+      config.base = '/ratex-ui/'
     }
 
     return config
